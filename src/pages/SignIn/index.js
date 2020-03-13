b05d48424b20e9c0df381415b7a0d48fde552ee3
Rafael Korz <rafael.korz@gmail.com>
2020-03-12 23:27:27 -0300

    Autenticação usuário

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import { signInRequest } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Invalid Email Format')
+    .required('Email is Required'),
+  password: Yup.string()
+    .min(6, 'password must have at least 6 characters in length')
+    .required('Password is Required')
+});
 
 export default function SignIn() {
-  return <div />;
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Type your Password"
+        />
+
+        <button type="submit">{loading ? 'Loading . . .' : 'Sign in'}</button>
+        <Link to="/register">Create Free Account</Link>
+      </Form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+import { signUpRequest } from '~/store/modules/auth/actions';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Name is Required'),
+  email: Yup.string()
+    .email('Invalid Email Format')
+    .required('Email is Required'),
+  password: Yup.string()
+    .min(6, 'Password must have at least 6 characters in length')
+    .required('Password is Required')
+});
 
 export default function SignUp() {
-  return <div />;
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full Name" />
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Password" />
+
+        <button type="submit">Create Account</button>
+        <Link to="/">Sign in</Link>
+      </Form>
+    </>
+  );
 }
