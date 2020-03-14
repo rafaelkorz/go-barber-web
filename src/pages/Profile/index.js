b0efe1923b57dd040d643759b31da7b02e122a55
Rafael Korz <rafael.korz@gmail.com>
2020-03-14 15:32:48 -0300

    Notificações e edição de profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  function handlerSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handlerSubmit}>
+        <Input name="name" placeholder="Nome campleto" />
+        <Input name="email" type="email" placeholder="Seu endereço de email" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha antiga"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmar senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
